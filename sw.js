3fe78f3f73a2b9ee985a1e6da1e2663597339f12
primape91913-a11y <primape91913@gmail.com>
2026-05-15 22:24:31 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,30 @@
-const CACHE_NAME = 'randomizer-v2';
-const ASSETS = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'randomizer-v3';
+const ASSETS = ['./index.html', './manifest.json'];
 
+// Install new cache and force it to take over immediately
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); 
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Delete all old, broken caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Network First: Always try to get the newest GitHub code first!
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
